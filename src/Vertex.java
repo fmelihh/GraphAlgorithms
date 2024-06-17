3ab38745d8ad5fe0f6c106c478cc3575e5c14395
fmelihh <furkanmelihercan.98@gmail.com>
2024-06-18 00:06:42 +0300

    feat: vertex class created.

---
+++ src/Vertex.java
@@ -0,0 +1,15 @@
+import java.util.ArrayList;
+
+public class Vertex {
+    private String data;
+    private ArrayList<Edge> edges;
+
+    public Vertex(String inputData) {
+        this.data = inputData;
+        this.edges = new ArrayList<Edge>();
+    }
+
+    public void addEdge(Vertex endVertex, Integer weight) {
+
+    }
+}
